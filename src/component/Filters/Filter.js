942f84f8980abd792f5cffe1ee136c833fd14fd6
JDuque <diegoduqqe@gmail.com>
2023-07-31 15:50:14 -0500

    Fix: reiniciar filtros

--- src/component/Filters/Filter.js
+++ src/component/Filters/Filter.js
@@ -1,21 +1,32 @@
 import React from "react";
-import { Grid, Typography, Box } from "@mui/material";
+import { Grid, Typography, Box, Button } from "@mui/material";
+import { Clear } from "@mui/icons-material";
 import Gender from "./Category/Gender";
 import Species from "./Category/Species";
 import Status from "./Category/Status";
-function Filter({ setStatus, setPageNumber, setGender, setSpecies }) {
+const Filter = ({ setStatus, setPageNumber, setGender, setSpecies }) => {
+  let clear = () => {
+    setStatus("");
+    setGender("");
+    setSpecies("");
+    setPageNumber("");
+    window.location.reload(false);
+  };
   return (
     <Grid item xs={3} sm={3}>
       <Typography fontWeight={"bold"} sx={{ mb: 4 }} textAlign={"center"}>
         Filter
       </Typography>
-      <Typography
-        sx={{ mb: 2, textDecoration: "underline" }}
-        textAlign="center"
-        style={{ cursor: "pointer" }}
+      <Button
+        variant="outlined"
+        startIcon={<Clear />}
+        onClick={() => {
+          clear();
+        }}
       >
         Clear Filter
-      </Typography>
+      </Button>
+
       <Box
         justifyContent={"end"}
         display={"flex"}
@@ -29,6 +40,6 @@ function Filter({ setStatus, setPageNumber, setGender, setSpecies }) {
       </Box>
     </Grid>
   );
-}
+};
 
 export default Filter;
